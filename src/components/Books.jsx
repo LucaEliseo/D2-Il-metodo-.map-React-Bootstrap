0d2511d20ddfa784bd2cdcc4ff7ecf02ad7bd7e4
LucaEliseo <lucaeliseo@outlook.it>
2023-03-29 16:32:25 +0200

    Commit Finale

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,28 +1,29 @@
-import { Component } from 'react'
+import { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import fantasy from '../Data/fantasy.json'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-
+import fantasy from '../Data/fantasy.json';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Books extends Component {
   render() {
     return (
-      <Container className='mt-3'> 
-        <h2 className='mt-5'>Libri Fantasy</h2>
-        <Row xs={1} sm={2} md={3} lg={4} xl={4} >
+      <Container className='mt-5'> 
+        <h2 className=' mb-5'>Libri Fantasy</h2>
+        <Row xs={1} sm={1} md={2} lg={3} xl={4} className='g-5'>
           {fantasy.map((item) => (
-            <Col  key={item.asin} className='justify-content-around'>
-              <Card className='ms-5 mt-3 w-100' style={{ width: '18rem' }}>
-                <Card.Img  variant="top" src={item.img} />
-                <Card.Body>
+            <Col key={item.asin} className='mb-4'>
+              <Card style={{ width: '18rem', height: '100%' }}>
+                <Card.Img variant='top' src={item.img} className='card-img-top' style={{ height: '350px' }} />
+                <Card.Body className='d-flex flex-column'>
                   <Card.Title>{item.title}</Card.Title>
                   <Card.Text>
                     {item.category} {item.price + '$'}
                   </Card.Text>
-                  <Button variant="primary">Acquista</Button>
+                  <div className='mt-auto'>
+                    <Button variant='primary'>Acquista</Button>
+                  </div>
                 </Card.Body>
               </Card>
             </Col>
@@ -34,3 +35,4 @@ class Books extends Component {
 }
 
 export default Books;
+
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -8,7 +8,7 @@ function Welcome() {
          className="d-block w-100" 
           src="https://www.lafeltrinelli.it/assets/2023/03/20230328091821-feltrinell1p1-slideshow-left-.webp" 
           alt="First slide"
-          height='700px'
+          height='600px'
         />
        
       </Carousel.Item>
@@ -17,7 +17,7 @@ function Welcome() {
           className="d-block w-100"
           src="https://www.lafeltrinelli.it/assets/2023/03/20230320094418-pennacmalaussenewelcome-.webp"
           alt="Second slide"
-          height='700px'
+          height='600px'
         />
       </Carousel.Item>
     </Carousel>
